136ec28e76f9c6d98755732887db4edeb4277b7c
Joanna Hao <j.hao3@share.epsb.ca>
2022-10-27 10:19:47 -0600

    Finished flowchart and up to all balancing.

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ author: Joanna Hao
 date-created: 2022-10-20
 """
 # ---------- VARIABLES ---------- #
-COL1_IONS = ("H", "Li", "Na", "K", "Rb", "Cs", "Fr", "NH4",   "NO3", "ClO3", "ClO4", "CH3COO")
-COL2_ION = "F"
-COL3_IONS = ("Cl", "Br", "I")
-COL4_ION = "SO4"
-COL5_7_IONS = ("CO3", "SO3", "PO4", "OH")
-COL6_IONS = ("IO3", "OOCCOO")
+POS1_IONS = ("H", "Li", "Na", "K", "Rb", "Cs", "Fr", "NH4", "Ag", "Tl")
+POS2_IONS = ("Be", "Mg", "Ca", "Sr", "Ba", "Ra", "Mn", "Co", "Ni", "Pd", "Cu", "Zn", "Cd", "Pb", "Po")
 
+MOLAR_MASSES = {
+    "H": 1.01, "Li": 6.94, "Na": 22.99, "K": 39.10, "Rb": 85.47, "Cs": 132.91, "Fr": 223,
+
+
+    "Pb": 207.2,
+
+    "F": 19.00, "Cl": 35.45, "Br": 79.90, "I": 126.90, "At": 210
+}
 
 # ---------- FUNCTIONS ---------- #
-def startScreen():
-    """
-    display start screen text
-    :return:
-    """
-
-
 # ----- Inputs
 def getReactants():
     """
@@ -31,44 +28,67 @@ def getReactants():
     for i in range(2):
         ion = input(f"Please enter the {charge[i]} reacting ion. (no charges) ")
         volume = input(f"What is the volume of the {charge[i]} solution? (L) ")
+        # check
         conc = input(f"What is the concentration of the {charge[i]} solution? (mol/L) ")
+        # check
         ion_info.append((ion, float(volume), float(conc)))
     return ion_info
 
 
 # ----- Processing
 def getCharge(elem):
-    """
-    get charge of element
-    :param elem: str (from tuple)
-    :return:
-    """
-    global COL1_IONS, COL2_ION, COL3_IONS, COL4_ION, COL5_7_IONS, COL6_IONS
-
-    # problematic: CO3, SO3
-    if elem in COL1_IONS[:8]:
+    global POS1_IONS, POS2_IONS  # , all the remaining charges pos & neg
+    if elem in POS1_IONS:
         return 1
-    elif elem in COL1_IONS[8:]:
-        return -1
-    elif elem in COL2_ION or elem in COL3_IONS or elem == COL5_7_IONS[-1]:
-        return -1
-    elif elem in COL4_ION or elem == COL5_7_IONS[:2] or elem == COL6_IONS[-1]:
-        return -2
-    elif elem == COL5_7_IONS[2]:
-        return -3
-    else:  # elem == COL6_IONS[0]  which is IO3 --> 1-
-        return -1
+    elif elem in POS2_IONS:
+        return 2
 
 
-def balanceEquation(elem1, elem2):
+def getMolarMass(elem):
+    global MOLAR_MASSES
+    return MOLAR_MASSES[elem]
+
+
+def GCF(a, b):
+    if b == 0:
+        return a
+    return GCF(b, a % b)
+
+def LCM(a, b):
+    g = GCF(a, b)
+    return a // g * b
+
+
+def balanceProduct(cation, anion, cation_charge, anion_charge):
     """
-    balance equation so that net charge of reactants, products is 0
-    :param ELEM1:
-    :param ELEM2:
+    determine balanced chemical formula of product
+    :param cation: str
+    :param anion: str
+    :param cation_charge: int
+    :param anion_charge: int
     :return:
     """
+    # FINDING PRODUCT
+    charge_magnitude = LCM(cation_charge, abs(anion_charge))
+    # neither subscript is going to have remainder b/c LCM w/ other was found
+    cation_subscript = charge_magnitude // cation_charge
+    anion_subscript = charge_magnitude // abs(anion_charge)
+    product = f"{cation}{cation_subscript}{anion}{anion_subscript}"
 
+    # FINDING COEFFICIENTS
+    # check if anion is diatomic (BrINClHOF)/polyatomic (P4, S8)
+    diatomics = ("Br", "I", "N", "Cl", "H", "O", "F")
+    polyatomics = {"P": "P4", "S": "S8"}
+    if anion in diatomics:
+        anion = f"{anion}2"
+    elif anion in polyatomics:
+        anion = polyatomics[anion]
 
+    coeff1 = cation_subscript  # cation always a single atom -- matches whatever the product cation subscript it
+    coeff2 = LCM(int(anion[-1]), anion_subscript) // int(anion[-1])
+    coeff3 = LCM(int(anion[-1]), anion_subscript) // anion_subscript
+    chem_equation = [coeff1, cation, coeff2, anion, coeff3, product]
+    return chem_equation
 
 
 def findLR(volume, conc, elem1_coeff, elem2_coeff):
@@ -93,6 +113,6 @@ def calcMass():
 
 # ---------- MAIN PROGRAM CODE ---------- #
 if __name__ == "__main__":
-    # IONS = getReactants()
+    IONS = getReactants()
     # print(IONS)
-    print(getCharge("OH"))
+    # print(getCharge("OH"))
